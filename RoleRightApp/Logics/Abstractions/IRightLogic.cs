569515a6f7648a3e2e8986dabb52ed574993d2bd
Danny <tnd.290800@gmail.com>
2022-09-07 13:20:03 +0700

    create feature add right

---
+++ RoleRightApp/Logics/Abstractions/IRightLogic.cs
@@ -0,0 +1,13 @@
+﻿using RoleRightApp.Repositories.Models;
+using RoleRightApp.RequestModels;
+
+namespace RoleRightApp.Logics.Abstractions
+{
+    public interface IRightLogic
+    {
+        Task<List<RightModel>> GetAllRight();
+        Task<RightModel> GetRight(string rightId);
+        Task<string> SaveRight(RightRequestModel rightModel);
+
+    }
+}
---
+++ RoleRightApp/Repositories/Implementations/RightRepository.cs
@@ -0,0 +1,33 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+using RoleRightApp.Repositories.Abstractions;
+using RoleRightApp.Repositories.Models;
+using RoleRightApp.RequestModels;
+
+namespace RoleRightApp.Repositories.Implementations
+{
+    public class RightRepository : HashKeyOnlyRepositoryBase<RightModel, string>, IRightRepository
+    {
+        public RightRepository(IDynamoDBContext dbContext) : base(dbContext)
+        {
+
+        }
+        public async Task<List<RightModel>> GetAllRight()
+        {
+            return await GetList();
+        }
+
+        public async Task<RightModel> GetRight(string rightId)
+        {
+            return await GetByHashKey(rightId);
+        }
+
+        public async Task<string> SaveRight(RightModel rightModel)
+        {
+            var rightId = Guid.NewGuid().ToString();
+            await SaveAsync(rightModel);
+
+            return rightId;
+        }
+
+    }
+}
