01701ebfcbc5b10bd0cb324b215d4bd68657ec1e
Hieu <trantrinhhieu2908@gmail.com>
2022-09-26 13:33:32 +0700

    refactor UploadFileAsync

--- RoleRightApp/Services/Implementations/StorageService.cs
+++ RoleRightApp/Services/Implementations/StorageService.cs
@@ -17,32 +17,29 @@ public class StorageService : IStorageService
 
     public async Task<string> UploadFileAsync(S3ObjectUpload s3RequestUpload)
     {
-        var path = S3Helper.GetS3ObjectKey(s3RequestUpload.Prefix, s3RequestUpload.File);
-        var bucketExists = false;
-        var errorMessage = string.Empty;
-
+        var result = string.Empty;
         try
         {
-            bucketExists = await _s3Client.DoesS3BucketExistAsync(s3RequestUpload.BucketName);
-            errorMessage = $"Bucket {s3RequestUpload.BucketName} does not exist.";
+            var path = S3Helper.GetS3ObjectKey(s3RequestUpload.Prefix, s3RequestUpload.File);
+            var bucketExists = await _s3Client.DoesS3BucketExistAsync(s3RequestUpload.BucketName);
+            if (bucketExists)
+            {
+                var request = new PutObjectRequest()
+                {
+                    BucketName = s3RequestUpload.BucketName,
+                    Key = path,
+                    InputStream = s3RequestUpload.File.OpenReadStream()
+                };
+                request.Metadata.Add("Content-Type", s3RequestUpload.File.ContentType);
+                await _s3Client.PutObjectAsync(request);
+                result = $"{s3RequestUpload.File.FileName} upload succesfully.";
+            }
+            result = $"Bucket {s3RequestUpload.BucketName} does not exist.";
         }
         catch(Exception ex)
         {
-            errorMessage = $"Fail to connect to S3";
+            result = $"S3 Error: {ex.Message}";
         }
-
-        if (bucketExists)
-        {
-            var request = new PutObjectRequest()
-            {
-                BucketName = s3RequestUpload.BucketName,
-                Key = path,
-                InputStream = s3RequestUpload.File.OpenReadStream()
-            };
-            request.Metadata.Add("Content-Type", s3RequestUpload.File.ContentType);
-            await _s3Client.PutObjectAsync(request);
-            return $"{s3RequestUpload.File.FileName} upload succesfully.";
-        }
-        return errorMessage;
+        return result;        
     }
 }
